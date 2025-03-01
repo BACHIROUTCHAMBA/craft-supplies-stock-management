8f6b8aa7c0e59879c16d2cca9e00556a744927a0
bachirou tchamba <tchambabachirou40@gmail.com>
2025-03-01 09:21:08 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/craftsuppliesstockmanagementapp/Craftsuppliesstockmanagementapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.craftsuppliesstockmanagementapp;
+
+/**
+ *
+ * @author Bachirou
+ */
+public class Craftsuppliesstockmanagementapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
